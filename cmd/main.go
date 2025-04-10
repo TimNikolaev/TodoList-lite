e2763be054bf91dad12391fd0184ff8a1d2639bc
TimNik <timnikolaev992@gmail.com>
2025-04-10 18:40:13 +0300

    Добваление пловного завершения роботы приложения.

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
 	"todo-std"
 	"todo-std/configs"
 	"todo-std/internal/handler"
@@ -31,10 +35,27 @@ func main() {
 	handler := handler.NewHandler(service)
 
 	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("port"), handler.InitRouts()); err != nil {
-		logrus.Fatalf("error occurred while running http server: %s", err.Error())
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handler.InitRouts()); err != nil {
+			logrus.Fatalf("error occurred while running http server: %s", err.Error())
+		}
+	}()
+
+	logrus.Print("TodoApp Started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Print("TodoApp Shotting Down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error occurred on server shutting down %s", err.Error())
 	}
 
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error occurred on db connection close")
+	}
 }
 
 func initConfig() error {
